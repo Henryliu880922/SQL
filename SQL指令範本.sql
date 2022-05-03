a8c6df33a8a83f13be22a2cdacd642c4d092e301
Henryliu880922 <es13463077@gmail.com>
2022-05-03 23:06:54 +0800

    Create SQL指令範本.sql

---
+++ SQL指令範本.sql
@@ -0,0 +1,36 @@
+insert [into] values
+insert into select from
+insert select 1 union select 2
+
+update set = where
+update set 1 = , n = where
+
+delete from where
+truncate table
+
+select from where
+order by asc desc
+
+select * from
+
+select from where
+
+select as from where
+
+select * from   where is null
+
+select as from
+
+select top from where
+
+select top percent from where
+
+select * from where like
+
+select * from where between and 
+
+select * from where in
+
+select from where
+
+select from inner join on 
